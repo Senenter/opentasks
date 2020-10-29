aff7a3665da76fb5c912ea78043e405b9c63f431
Mart de Jager <jage0030@hz.nl>
2020-10-29 13:42:54 +0100

    changed onswap namne

--- testDelete.java
+++ testDelete.java
@@ -4,12 +4,12 @@
      * {@link com.jmedeisis.draglinearlayout.DragLinearLayout#setOnViewSwapListener(com.jmedeisis.draglinearlayout.DragLinearLayout.OnViewSwapListener)} to
      * listen for draggable view swaps.
      */
-    public interface OnViewSwapListener
+    public interface notOnViewSwapListener
     {
         /**
          * Invoked right before the two items are swapped due to a drag event. After the swap, the firstView will be in the secondPosition, and vice versa.
          * <p/>
          * No guarantee is made as to which of the two has a lesser/greater position.
          */
-        void onSwap(View firstView, int firstPosition, View secondView, int secondPosition);
+        void notOnSwap(View firstView, int firstPosition, View secondView, int secondPosition);
     }
