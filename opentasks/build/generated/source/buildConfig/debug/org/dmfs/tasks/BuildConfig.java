b4bcc98d3ef56ac36f784fd993b3741aa53b3b4f
Mart de Jager <jage0030@hz.nl>
2020-10-30 15:49:33 +0100

    added build files

---
+++ opentasks/build/generated/source/buildConfig/debug/org/dmfs/tasks/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.dmfs.tasks;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.dmfs.tasks";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 86074;
+  public static final String VERSION_NAME = "v1.3.4-26-gfd0c953d-dirty";
+}
---
+++ opentasks/build/generated/source/buildConfig/release/org/dmfs/tasks/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.dmfs.tasks;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "org.dmfs.tasks";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 79701;
+  public static final String VERSION_NAME = "1.2.4-13-g08162d7-dirty";
+}
